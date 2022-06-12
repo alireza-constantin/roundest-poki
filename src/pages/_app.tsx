567802107f3f912f4d064c20997e3d8a16aadaf2
Alireza Soheili <71215827+alireza-constantin@users.noreply.github.com>
2022-06-12 15:07:53 +0430

    make trpc up and running

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,23 @@ import type { AppProps } from 'next/app';
 import 'tailwindcss/tailwind.css';
 import '../styles/global.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
-}
+import { withTRPC } from '@trpc/next';
+import type { AppType } from 'next/dist/shared/lib/utils';
+import type { AppRouter } from '@/backend/router';
 
-export default MyApp;
+const MyApp: AppType = ({ Component, pageProps }) => {
+	return <Component {...pageProps} />;
+};
+
+export default withTRPC<AppRouter>({
+	config({ ctx }) {
+		const url = process.env.VERCEL_URL
+			? `https://${process.env.VERCEL_URL}/api/trpc`
+			: 'http://localhost:3000/api/trpc';
+
+		return {
+			url,
+		};
+	},
+	ssr: true,
+})(MyApp);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,12 @@
+import { trpc } from '@/utils/trpc';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
+	const { data, isLoading } = trpc.useQuery(['hello', { text: 'alireza' }]);
+
+	if (isLoading) return <div>Loading...</div>;
+	if (data) return <div>{data.greeting}</div>;
+
 	return (
 		<div className="h-screen w-screen flex flex-col justify-center items-center">
 			<div className="text-2xl text-center mb">Wich Pokémon is the Roundest?</div>
